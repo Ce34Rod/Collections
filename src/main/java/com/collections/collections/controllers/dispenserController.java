d70e308ce66f0b963200d3b105698dc57ff807db
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-09-02 21:17:05 -0500

    added dispenserController

---
+++ src/main/java/com/collections/collections/controllers/dispenserController.java
@@ -0,0 +1,16 @@
+package com.collections.collections.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class dispenserController {
+
+    @GetMapping("'")
+    public String getDispenserPage() {
+        return "dispenser.html";
+    }
+
+}
